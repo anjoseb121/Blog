42745a7b3ce1284a4c1cb349af7deabc50abf878
AntonioBar <anjose.121@gmail.com>
2017-02-22 09:24:56 -0500

    Added concerns permission for user

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170217125832) do
+ActiveRecord::Schema.define(version: 20170217204028) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
@@ -65,6 +65,7 @@ ActiveRecord::Schema.define(version: 20170217125832) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.integer  "permission_level",       default: 1
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
