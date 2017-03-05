dfda382e9c4a4b22bbcb61d27c0563acf3e9daf1
AntonioBar <anjose.121@gmail.com>
2017-03-05 03:42:42 -0500

    Added render for comments, style to article view, gravatar for comments

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+	def article_cover url, options = {}
+		html_class = options[:class]
+		html_style = style="background:url(#{url}); background-size: cover;"\
+							"height:400px; width: 100%;"
+		html = "<header style='#{html_style}' class='#{html_class}'> </header>"
+		html.html_safe
+	end
 end
